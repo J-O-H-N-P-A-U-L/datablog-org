ae9d7a1e3f94c28a418023a9507d0194168fe38f
J-O-H-N-P-A-U-L <jp.hunter@aol.com>
2023-09-13 15:48:31 +1000

    update projects and such

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,39 @@
-import * as React from "react";
-import { Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout";
-import Seo from "../components/seo";
-import * as styles from "../components/index.module.css";
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
 
-const blogPosts = [
+const projects = [
   {
-    title: "Unlocking Insights with Data Analysis",
-    excerpt:
-      "Join me on a journey through the world of data analysis, where we uncover hidden patterns and make data-driven decisions. Learn how Python, Pandas, and Matplotlib play pivotal roles in this adventure.",
-    postUrl: "https://datablog.org/unlocking-insights-data-analysis",
+    name: "Jupyter Code Llama Project",
+    description:
+      "Step into the world of Jupyter notebooks with the Jupyter Code Llama Project. Explore my data analysis and coding skills as I work on various projects using Jupyter notebooks and Python. Get ready to code like a pro!",
+    repositoryUrl: "https://github.com/J-O-H-N-P-A-U-L/jupyter-code-llama",
   },
   {
-    title: "Building Dynamic Web Applications with MERN Stack",
-    excerpt:
-      "Explore the fascinating realm of full-stack web development with me as we delve into Project A. Discover how MongoDB, Express.js, React, and Node.js come together to create powerful web applications.",
-    postUrl: "https://datablog.org/mern-stack-web-development",
+    name: "AIPND Project",
+    description:
+      "Bringing AI to life with the AIPND Project! Dive into the world of artificial intelligence as I tackle a fascinating project. Explore the code and see how I navigate the complexities of AI and machine learning.",
+    repositoryUrl: "https://github.com/J-O-H-N-P-A-U-L/aipnd-project",
   },
   {
-    title: "Inspire Your Day with React Native: Project C",
-    excerpt:
-      "Join me in exploring the world of mobile app development as we dive into Project C. This React Native-based app is designed to uplift and motivate with daily quotes and images.",
-    postUrl: "https://datablog.org/inspire-your-day-react-native",
+    name: "R Shiny NYPD Felonies Project",
+    description:
+      "Explore crime data like never before with the R Shiny NYPD Felonies Project. This project demonstrates my data analysis and visualization skills using R Shiny. Dive into crime statistics in an interactive way.",
+    repositoryUrl: "https://github.com/J-O-H-N-P-A-U-L/r-shiny-nypd-felonies",
   },
-];
+  {
+    name: "Flood Forecasting Project",
+    description:
+      "Join me on a mission to save lives and properties! The Flood Forecasting Project is my contribution to mitigating flood risks. I've leveraged data analysis and predictive modeling to build a tool that can help forecast potential flood events.",
+    repositoryUrl: "https://github.com/J-O-H-N-P-A-U-L/Flood_forecasting",
+  },
+]
 
-const DatablogHomepage = () => (
+const IndexPage = () => (
   <Layout>
     <div className={styles.textCenter}>
       <StaticImage
@@ -40,24 +46,24 @@ const DatablogHomepage = () => (
         style={{ marginBottom: `var(--space-3)` }}
       />
       <h1>
-        Datablog.org - John Paul's Data Adventures
+        Discover My World of Coding Adventures!
       </h1>
       <p className={styles.intro}>
-        Welcome to Datablog.org, where I, John Paul Hunter, share my exciting adventures in the world of data. Join me on a journey filled with data analysis, web development, and mobile app creation. Explore my latest blog posts below.
+        Hey there! I'm John Paul, and I thrive in the realm of code. Join me on a journey through some of my most exciting GitHub projects. From dynamic web apps to data wizardry, Jupyter notebooks, interactive crime data visualization, and even flood forecasting, it's all here.
       </p>
     </div>
     <ul className={styles.list}>
-      {blogPosts.map((post) => (
-        <li key={post.postUrl} className={styles.listItem}>
-          <h2>{post.title}</h2>
-          <p className={styles.listItemDescription}>{post.excerpt}</p>
+      {projects.map((project) => (
+        <li key={project.repositoryUrl} className={styles.listItem}>
+          <h2>{project.name}</h2>
+          <p className={styles.listItemDescription}>{project.description}</p>
           <a
             className={styles.listItemLink}
-            href={post.postUrl}
+            href={project.repositoryUrl}
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read More ↗
+            Dive In ↗
           </a>
         </li>
       ))}
@@ -70,6 +76,6 @@ const DatablogHomepage = () => (
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Datablog.org - John Paul's Data Adventures" />
+export const Head = () => <Seo title="John Paul Hunter's Projects" />
 
-export default DatablogHomepage;
+export default IndexPage
